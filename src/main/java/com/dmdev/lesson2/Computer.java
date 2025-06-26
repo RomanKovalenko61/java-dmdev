18130b6521d165be546a6fc91ed1d728e979135d
Roman <astek14@mail.ru>
2025-06-26 04:55:26 +0300

    lesson 3 Constructors

--- src/main/java/com/dmdev/lesson2/Computer.java
+++ src/main/java/com/dmdev/lesson2/Computer.java
@@ -3,7 +3,21 @@ package com.dmdev.lesson2;
 public class Computer {
 
     int ssd = 500;
-    int ram = 1024;
+    int ram;
+
+    Computer() {
+        System.out.println("Я был создан");
+    }
+
+    Computer(int newSsd) {
+        ssd = newSsd;
+        ram = 999;
+    }
+
+    Computer(int ssd, int newRam) {
+        this.ssd = ssd;
+        ram = newRam;
+    }
 
     void load() {
         System.out.println("Я загрузился");
@@ -12,5 +26,6 @@ public class Computer {
     void printState() {
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("RAM: " + ram + " MB");
+        System.out.println();
     }
 }
