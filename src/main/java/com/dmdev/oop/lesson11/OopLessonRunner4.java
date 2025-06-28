76c343f1bed3d7eb222bb30f833f441a7eab7691
Roman <astek14@mail.ru>
2025-06-28 13:02:55 +0300

    interfaces - new features

--- src/main/java/com/dmdev/oop/lesson11/OopLessonRunner4.java
+++ src/main/java/com/dmdev/oop/lesson11/OopLessonRunner4.java
@@ -6,7 +6,7 @@ public class OopLessonRunner4 {
         Printable laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
         Printable mobile = new Mobile(new Ssd(125), new Ram(512));
 
-        print(laptop, mobile);
+        printWithRandom(laptop, mobile);
         System.out.println();
         System.out.println("Interface value " + Printable.SOME_VALUE);
     }
@@ -14,6 +14,13 @@ public class OopLessonRunner4 {
     public static void print(Printable... objects) {
         for (Printable object : objects) {
             object.print();
+            System.out.println();
+        }
+    }
+
+    public static void printWithRandom(Printable... objects) {
+        for (Printable object : objects) {
+            object.printWithRandom();
             System.out.println();
         }
     }
--- src/main/java/com/dmdev/oop/lesson11/Printable.java
+++ src/main/java/com/dmdev/oop/lesson11/Printable.java
@@ -1,8 +1,20 @@
 package com.dmdev.oop.lesson11;
 
+import java.util.Random;
+
 public interface Printable {
 
     String SOME_VALUE = "Example";
+    Random random = new Random();
+
+    default void printWithRandom() {
+        System.out.println("Random value " + generateRandom());
+        print();
+    }
 
     void print();
+
+    private int generateRandom() {
+        return random.nextInt();
+    }
 }
