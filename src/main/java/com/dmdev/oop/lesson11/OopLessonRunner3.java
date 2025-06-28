9b82ed4a110c22972bc1f6b2c5fd316352be720d
Roman <astek14@mail.ru>
2025-06-28 12:26:42 +0300

    instance of

--- src/main/java/com/dmdev/oop/lesson11/OopLessonRunner3.java
+++ src/main/java/com/dmdev/oop/lesson11/OopLessonRunner3.java
@@ -6,14 +6,19 @@ public class OopLessonRunner3 {
         Computer laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
         Computer mobile = new Mobile(new Ssd(125), new Ram(512));
 
-        loadComputers(laptop, mobile);
+        var laptop1 = new Laptop(new Ssd(5000), new Ram(10000), 5);
+
+//        loadComputers(laptop, mobile);
         System.out.println();
-        printInfo(new Computer[]{laptop, mobile});
+        printInfo(new Computer[]{laptop, mobile, laptop1});
     }
 
     public static void printInfo(Computer[] computers) {
         for (Computer computer : computers) {
             computer.print();
+            if (computer instanceof Laptop) {
+                ((Laptop) computer).open();
+            }
             System.out.println();
         }
     }
