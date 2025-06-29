93dc77369a2c4a07f33d310edfbebc905d089abe
Roman <astek14@mail.ru>
2025-06-29 15:40:27 +0300

    Generics theory part 2

--- src/main/java/com/dmdev/oop/lesson18/WeaponRunner.java
+++ src/main/java/com/dmdev/oop/lesson18/WeaponRunner.java
@@ -1,9 +1,11 @@
 package com.dmdev.oop.lesson18;
 
 import com.dmdev.oop.lesson15.Archer;
+import com.dmdev.oop.lesson15.Hero;
 import com.dmdev.oop.lesson15.Warrior;
 import com.dmdev.oop.lesson18.weapon.Bow;
 import com.dmdev.oop.lesson18.weapon.Sword;
+import com.dmdev.oop.lesson18.weapon.Weapon;
 
 public class WeaponRunner {
 
@@ -13,5 +15,13 @@ public class WeaponRunner {
 
         Warrior<Sword> warrior = new Warrior<>("Боромир", 10);
         warrior.setWeapon(new Sword());
+
+        printWeaponDamage(archer);
+        printWeaponDamage(warrior);
+    }
+
+    //                                                      Hero<? extends Weapon>  Hero<? super Sword>
+    public static <T extends Weapon> void printWeaponDamage(Hero<T> hero) {
+        System.out.println(hero.getWeapon().getDamage());
     }
 }
