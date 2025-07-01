bce753c5b416ff831778d6f771017a838ee53701
Roman <astek14@mail.ru>
2025-07-01 12:18:20 +0300

    hashmap practice

--- src/main/java/com/dmdev/oop/lesson20/map/MapExample.java
+++ src/main/java/com/dmdev/oop/lesson20/map/MapExample.java
@@ -10,11 +10,30 @@ public class MapExample {
     public static void main(String[] args) {
         var ivan = new Person(1, "Ivan", "Ivanov");
         var petr = new Person(2, "Petrov", "Petrov");
+        var sveta = new Person(1, "Sveta", "Svetikova");
 
         Map<Integer, Person> map = new HashMap<>();
         map.put(ivan.getId(), ivan);
         map.put(petr.getId(), petr);
+        map.putIfAbsent(sveta.getId(), sveta);
 
-        System.out.println(map.get(2));
+//        System.out.println(map.get(2));
+//
+//        System.out.println(map.keySet());
+//        System.out.println(map.values());
+//        System.out.println(map.entrySet());
+//
+//        for (Person person : map.values()) {
+//            System.out.println(person.getFirstName());
+//        }
+
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        System.out.println(map.containsKey(4));
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.getOrDefault(6, sveta));
     }
 }
