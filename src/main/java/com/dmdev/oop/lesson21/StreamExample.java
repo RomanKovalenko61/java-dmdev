ebc56f6b7145cd2484cf9a66dd4dc163e3a8f65b
Roman <astek14@mail.ru>
2025-07-02 13:21:31 +0300

    Stream for primitives

--- src/main/java/com/dmdev/oop/lesson21/StreamExample.java
+++ src/main/java/com/dmdev/oop/lesson21/StreamExample.java
@@ -1,20 +1,38 @@
 package com.dmdev.oop.lesson21;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExample {
 
     public static void main(String[] args) {
-        List<String> strings = List.of("88","11", "22", "33", "44", "55", "66");
+        List<String> strings = List.of("88", "11", "22", "33", "44", "55", "66");
 
-        strings.stream()
+        var intSummaryStatistics = strings.stream()
                 .map(string -> string + string)
                 .map(Integer::valueOf)
                 .filter(value -> value % 2 == 0)
                 .sorted()
-                .skip(1)
+//                .skip(1)
                 .limit(2)
-                .forEach(System.out::println);
+                .mapToInt(Integer::intValue) // convert to int
+//                .mapToObj(Integer::valueOf)
+                .summaryStatistics();
+        System.out.println(intSummaryStatistics);
+
+        List<String> list = Stream.of("88", "11", "22", "33", "44", "55", "66")
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        var sum = IntStream.of(1, 4, 5, 6).sum();
+        System.out.println(sum);
+
+        IntStream.range(0, 10).forEach(System.out::println); // 0...9
+        IntStream.rangeClosed(0, 10).forEach(System.out::println); // 0 ... 10
+
+        IntStream.iterate(0, i -> i + 3).skip(10).limit(10).forEach(System.out::println);
 
 //        for (String string : strings) {
 //            String value = string + string;
