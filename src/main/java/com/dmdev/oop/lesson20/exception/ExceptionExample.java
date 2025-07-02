914e454973703db3b10d5b3c34efcb42109192e4
Roman <astek14@mail.ru>
2025-07-02 09:52:04 +0300

    exception theory part 2

--- src/main/java/com/dmdev/oop/lesson20/exception/ExceptionExample.java
+++ src/main/java/com/dmdev/oop/lesson20/exception/ExceptionExample.java
@@ -1,25 +1,36 @@
 package com.dmdev.oop.lesson20.exception;
 
-import java.io.FileNotFoundException;
-
 public class ExceptionExample {
 
     public static void main(String[] args) {
         System.out.println("main start");
         try {
+            System.out.println(finallyTest());
             unsafe(10);
-        } catch (Exception e) {
-            // handle exception
+        } catch (RuntimeException e) {
+            System.out.println("catch");
             e.printStackTrace();
+        } finally {
+            System.out.println("finally");
         }
         System.out.println("main end");
         // code
     }
 
-    public static void unsafe(int value) throws FileNotFoundException {
+    public static int finallyTest() {
+        try {
+            return 2;
+        } catch (Throwable throwable) {
+            return 3;
+        } finally {
+            return 4;
+        }
+    }
+
+    public static void unsafe(int value) {
         System.out.println("unsafe start");
         if (value > 0) {
-            throw new FileNotFoundException();
+            throw new RuntimeException();
         }
         System.out.println("unsafe end");
         // code
