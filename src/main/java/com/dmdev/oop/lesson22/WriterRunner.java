b5abd040d03ceead00b8232d82674a01cd67e2a5
Roman <astek14@mail.ru>
2025-07-06 12:00:48 +0300

    Class Files

--- src/main/java/com/dmdev/oop/lesson22/WriterRunner.java
+++ src/main/java/com/dmdev/oop/lesson22/WriterRunner.java
@@ -1,19 +1,22 @@
 package com.dmdev.oop.lesson22;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class WriterRunner {
 
     public static void main(String[] args) throws IOException {
-        File file = Path.of("resources", "writer.poem").toFile();
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
-            writer.append("Hello world!");
-            writer.newLine();
-            writer.append("Java");
-        }
+        Path path = Path.of("resources", "writer2.poem");
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+
+        Files.write(path, List.of("Hello world!", "Java"));
+
+//        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+//            writer.append("Hello world!");
+//            writer.newLine();
+//            writer.append("Java");
+//        }
     }
 }
