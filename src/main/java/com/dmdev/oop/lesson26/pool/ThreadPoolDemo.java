2b99c1a5ba6c6084e6a4af4d694625344e1e3e52
Roman <astek14@mail.ru>
2025-08-08 17:15:26 +0300

    Callable Runnable Future

--- src/main/java/com/dmdev/oop/lesson26/pool/ThreadPoolDemo.java
+++ src/main/java/com/dmdev/oop/lesson26/pool/ThreadPoolDemo.java
@@ -1,15 +1,34 @@
 package com.dmdev.oop.lesson26.pool;
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ThreadPoolDemo {
 
-    public static void main(String[] args) {
-        Executors.newSingleThreadExecutor();
-        Executors.newFixedThreadPool(5);
-        Executors.newCachedThreadPool();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        var threadPool = Executors.newScheduledThreadPool(4);
+        threadPool.scheduleAtFixedRate(() -> {
+            System.out.println("OK!");
+        }, 2L, 4L, TimeUnit.SECONDS);
 
-        Executors.newScheduledThreadPool(3);
-        Executors.newWorkStealingPool();
+//        threadPool.shutdown();
+//        threadPool.awaitTermination(11, TimeUnit.HOURS);
+
+    }
+
+    private static void test() throws InterruptedException, ExecutionException {
+        var threadPool = Executors.newFixedThreadPool(5);
+        var future = threadPool.submit(() -> {
+            Thread.sleep(2000);
+            System.out.println("it's callable");
+            return 1;
+        });
+
+        CompletableFuture.runAsync(() -> {});
+
+
+        System.out.println("Result: " + future.get());
+        threadPool.shutdown();
+        threadPool.awaitTermination(1L, TimeUnit.HOURS);
+        System.out.println("main end");
     }
 }
